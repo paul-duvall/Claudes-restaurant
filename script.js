d68e990f6e0000466de60c700252daabe2d2ee7b
Paul Duvall <duvallpj@gmail.com>
2018-10-24 21:09:17 +0100

    Initial upload of index, css and js files

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  const heroLogo = document.querySelector('.hero_logo');
+  const heroText = document.querySelector('.fancy_text');
+
+  $(heroLogo).fadeIn(3000);
+  $(heroText).fadeIn(5000);
+
+});
