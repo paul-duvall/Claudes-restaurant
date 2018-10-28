f1ec13e9d4dbd93090e5a0d9cf30d7d186bace1f
Paul Duvall <duvallpj@gmail.com>
2018-10-28 12:21:53 +0000

    Awards and menus sections added to main page

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-  const heroLogo = document.querySelector('.hero_logo');
-  const heroText = document.querySelector('.fancy_text');
-
-  $(heroLogo).fadeIn(3000);
-  $(heroText).fadeIn(5000);
-
-});
