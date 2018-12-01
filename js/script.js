7b4ad2692af7e587f9f1f9d2e1292bb1cce29a03
Paul Duvall <duvallpj@gmail.com>
2018-12-01 14:13:02 +0000

    New pages added, bugs fixed on homepage, menu page build started

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(function(){
 
   const heroLogo = document.querySelector('.hero_logo');
   const heroText = document.querySelector('.fancy_text');
-  const menu = $('#menu');
+  // const menu = $('#menu');
   const slickMenuBtn = document.getElementsByClassName('.slicknav_menu');
 
-  console.log(slickMenuBtn);
+  // console.log(slickMenuBtn);
 
   $(heroLogo).fadeIn(3000);
   $(heroText).fadeIn(5000);
@@ -14,15 +14,25 @@ $(function(){
     label:'',
     duration: 400,
   });
-
-  // $('.slicknav_menu').click(function(event){
-  //   event.preventDefault();
-  //   $('.section--top').css("background-color", "red");
-  //   if($('.slicknav_btn').hasClass('.slicknav_open')) {
-  //     $('.section--top').css("background-color", "red");
-  //   } else {
-
-  //   }
-  // });
-
 });
+
+function openMenu(evt, menuName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
